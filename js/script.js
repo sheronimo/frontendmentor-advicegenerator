7030800483a0c7b1f3589ebc17a61fb6e41fb15c
Shuber <shuberinu@gmail.com>
2022-05-24 11:30:00 +0700

    fetch API script

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const endpoint = 'https://api.adviceslip.com/advice';
+const button = document.querySelector('#btn-getquote');
+
+const id = document.querySelector('#advice-id');
+const quote = document.querySelector('#advice-text');
+
+button.addEventListener('click', () => {
+	getQuote();
+	button.disabled = true;
+	setTimeout(() => {
+		button.disabled = false;
+	}, 2000);
+});
+
+function getQuote() {
+	fetch(endpoint, { cache: 'no-cache' })
+		.then((response) => {
+			console.log(response);
+			return response.json();
+		})
+		.then((data) => {
+			id.innerText = data.slip.id;
+			quote.innerText = data.slip.advice;
+		});
+}
+
+getQuote();
